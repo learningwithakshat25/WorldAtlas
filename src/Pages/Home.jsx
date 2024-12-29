b15208053e130d6164f1f2bd6cc2d35f6cba1387
learningwithakshat <2004mittalakshat@gmail.com>
2024-12-29 15:25:20 +0530

    Contact page  added in Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../Components/UI/HeroSection'
 import About from './About'
+import Contact from './Contact'
 
 const Home = () => {
   return (
     <div className='container mx-auto flex flex-col md:gap-12'>
     <HeroSection/>
     <About/>
+    <Contact/>
     </div>
     
   )
