99bb646862b69a1ce3d53e19a48fe539a1f10f57
learningwithakshat <2004mittalakshat@gmail.com>
2024-12-29 19:39:35 +0530

    Hamberger Update

--- src/Components/UI/Headers.jsx
+++ src/Components/UI/Headers.jsx
@@ -11,14 +11,14 @@ const Headers = () => {
       <NavLink to='/'>
         <h1 className='text-xl font-semibold'>WorldAtlas</h1>
       </NavLink>
-      {/* <div className={`mobile-nav-items`}>
-        <ul className={`inset-0 w-screen p-4  bg-stone-800 flex gap-4 flex-col absolute top-14 left-0 ${isOpenMenu ? "opacity-100" : "opacity-0"}`}>
+      <div className={`mobile-nav-items`}>
+        <ul className={`inset-0 w-screen p-4  bg-stone-800 flex gap-4 flex-col absolute top-14 left-0 ${isOpenMenu ? "flex" : "hidden"}`}>
         <li><NavLink className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='/'>Home</NavLink></li>
         <li><NavLink className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='About'>About</NavLink></li>
           <li><NavLink className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='Country'>Country</NavLink></li>
           <li><NavLink className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='Contact'>Contact</NavLink></li>
           </ul>
-          </div> */}
+          </div>
       <div className={`pc-nav-items`}>
         <ul className={"hidden md:flex flex-row gap-4"}>
           <li><NavLink className="text-md text-white" to='/'>Home</NavLink></li>
