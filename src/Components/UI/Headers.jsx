b48df0a7d8540018b747cce05db6340a23844b0e
learningwithakshat <2004mittalakshat@gmail.com>
2024-12-30 19:37:13 +0530

    Update Header

--- src/Components/UI/Headers.jsx
+++ src/Components/UI/Headers.jsx
@@ -1,38 +1,43 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Headers = () => {
-  const [isOpenMenu, setisOpenMenu] = useState(false);
+const Headers = ()=> {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-  <div className="navbar bg-stone-800">
+    <nav className="navbar bg-stone-800">
+      <header className='container mx-auto flex flex-row items-center justify-between p-4 text-white '>
+        <NavLink to='/'>
+          <h1 className='text-xl font-semibold'>WorldAtlas</h1>
+        </NavLink>
+        {/* Pc Nav-Items */}
+        <div className={`pc-nav-items`}>
+         <ul className={"hidden md:flex flex-row gap-4"}>
+           <li><NavLink className="text-md text-white" to='/'>Home</NavLink></li>
+           <li><NavLink className="text-md text-white" to='About'>About</NavLink></li>
+           <li><NavLink className="text-md text-white" to='Country'>Country</NavLink></li>
+           <li><NavLink className="text-md text-white" to='Contact'>Contact</NavLink></li>
+         </ul>
+       </div>
+       <button className="hamberger md:hidden" onClick={toggleMenu}>
+          <i className="fa-solid fa-bars text-xl"></i>
+        </button>
 
-    <header className='container mx-auto flex flex-row items-center justify-between p-4  text-white '>
-      <NavLink to='/'>
-        <h1 className='text-xl font-semibold'>WorldAtlas</h1>
-      </NavLink>
-      <div className={`mobile-nav-items`}>
-        <ul className={`inset-0 w-screen p-4  bg-stone-800 flex gap-4 flex-col absolute top-14 left-0 ${isOpenMenu ? "flex" : "hidden"}`}>
-        <li><NavLink target='blank' className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='/'>Home</NavLink></li>
-        <li><NavLink target='blank' className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='About'>About</NavLink></li>
-          <li><NavLink target='blank' className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='Country'>Country</NavLink></li>
-          <li><NavLink target='blank' className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='Contact'>Contact</NavLink></li>
+        {isOpen && (
+          <ul className='inset-0 w-screen p-4  bg-stone-800 flex gap-4 flex-col absolute top-14 left-0'>
+            <li><NavLink className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='/'>Home</NavLink></li>
+            <li><NavLink className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='/about'>About</NavLink></li>
+            <li><NavLink className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='/country'>Country</NavLink></li>
+            <li><NavLink className='hover:underline underline-offset-8 text-white font-semibold text-xl' to='/contact'>Contact</NavLink></li>
           </ul>
-          </div>
-      <div className={`pc-nav-items`}>
-        <ul className={"hidden md:flex flex-row gap-4"}>
-          <li><NavLink className="text-md text-white" to='/'>Home</NavLink></li>
-          <li><NavLink className="text-md text-white" to='About'>About</NavLink></li>
-          <li><NavLink className="text-md text-white" to='Country'>Country</NavLink></li>
-          <li><NavLink className="text-md text-white" to='Contact'>Contact</NavLink></li>
-        </ul>
-      </div>
-      <div onClick={() => { setisOpenMenu(!isOpenMenu) }} className="hamberger md:hidden">
-        <i className="fa-solid fa-bars text-xl"></i>
-      </div>
-    </header>
-  </div>
-  )
+        )}
+      </header>
+    </nav>
+  );
 }
 
-export default Headers
+export default Headers;
